164ff0a2b1b3f7f505c5065f4457b33aff228fbb
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-28 19:28:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,11 +6,12 @@ require 'pry'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name")
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry 
+   
 end
 
 create_project_hash
