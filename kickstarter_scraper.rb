22a4c623c3f304ea09164df0d247ac61ae6ae468
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-28 19:34:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,6 +14,16 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   
+  projects = {}
+ 
+  # Iterate through the projects
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    
+  end
+ 
+  # return the projects hash
+  projects
    
 end
 
